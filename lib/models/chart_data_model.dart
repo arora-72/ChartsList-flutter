0cc7078e2deb053ec0586d8bc6b03617ed8221d3
arora-72 <indresh.arora2016@vitstudent.ac.in>
2020-04-10 16:54:53 +0530

    fetch data firebase success

--- lib/models/chart_data_model.dart
+++ lib/models/chart_data_model.dart
@@ -7,14 +7,14 @@ class AssessmentRecord{
 
   final String assessmentTitle;
   final List<String> names;
-  final List<String> marks;
+  final List<int> marks;
   final DocumentReference documentReference;
 
   AssessmentRecord.fromMap(Map<String, dynamic>data, {this.documentReference})
   :
     assessmentTitle = data['title'],
     names = new List<String>.from(data['names']),
-    marks = new List<String>.from(data['marks']);
+    marks = new List<int>.from(data['marks']);
 
   AssessmentRecord.fromSnapshot(DocumentSnapshot snapshot):
       this.fromMap(snapshot.data, documentReference: snapshot.reference);
--- lib/pages/chart_list_page.dart
+++ lib/pages/chart_list_page.dart
@@ -16,46 +16,75 @@ class _ChartListViewState extends State<ChartListView> {
   final databaseReference = FirebaseDatabase.instance.reference();
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
-    _getData();
+
   }
-
-
-
-  Stream<AssessmentRecord> getData(){
-    return Firestore.instance
-        .collection("results")
-        .snapshots()
-  }
-
 
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-        home: Scaffold(
-            body: Center(
-                child: FlatButton(
-                  color: Colors.amber,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text("get Game Record"),
-                      StreamBuilder<AssessmentRecord>(
-                        stream: getGame(),
-                        builder: (BuildContext c, AsyncSnapshot<GameRecord> data) {
-                          if (data?.data == null) return Text("Error");
+      home: Scaffold(
+      appBar: new AppBar(title: Text('display dataa'),),
+      body: _buildBody(context),
+    ),
+    );
+  }
 
-                          GameRecord r = data.data;
 
-                          return Text("${r.creationTimestamp} + ${r.name}");
-                        },
-                      ),
-                    ],
-                  ),
-                  onPressed: () {
-                    getGame();
-                  },
-                ))));
+  Widget _buildBody(BuildContext context) {
+    // TODO: get actual snapshot from Cloud Firestore
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('results').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
+
+
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = AssessmentRecord.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.names),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.names.toString(),
+            style: new TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                textBaseline: TextBaseline.alphabetic
+            ),),
+          subtitle: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Text(record.marks.toString(),
+              style: new TextStyle(
+                  fontSize: 15.0,
+                  textBaseline: TextBaseline.alphabetic
+              ),),
+          ),
+          onTap: () => print(record),
+        ),
+      ),
+    );
   }
 }
